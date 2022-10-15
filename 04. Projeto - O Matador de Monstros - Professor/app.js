92661b71c3447ff1d9d06d8cf09bed1f93ce2926
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-15 15:27:05 -0300

    Implementando Atacar

--- 04. Projeto - O Matador de Monstros - Professor/app.js
+++ 04. Projeto - O Matador de Monstros - Professor/app.js
@@ -15,9 +15,25 @@ new Vue({
             this.running = false;
             this.playerLife = 100;
             this.monsterLife = 100;
+        },
+        attack(especial) {
+            this.hurt('monsterLife', 5, 10, especial);
+            this.hurt('playerLife', 7, 12, false);
+        },
+        hurt(atr, min, max, especial) {
+            const plus = especial ? 5 : 0;
+            const hurt = this.getRandom(min + plus, max + plus);
+            this[atr] = Math.max(this[atr] - hurt, 0);
+        },
+        geRandom(min, max) {
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value);
         }
     },
     watch: {
-
+        hasResult(value) {
+            if (value)
+                this.running = false;
+        }
     }
 });
