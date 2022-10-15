38d374d4f16923f37f4a6096c1e78336e1bb0e63
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-15 16:50:04 -0300

    Adicionando o Registro de Log

--- 04. Projeto - O Matador de Monstros - Professor/app.js
+++ 04. Projeto - O Matador de Monstros - Professor/app.js
@@ -3,7 +3,8 @@ new Vue({
 	data: {
         running: false,
         playerLife: 100,
-        monsterLife: 100
+        monsterLife: 100,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -12,30 +13,38 @@ new Vue({
     },
 	methods: {
         startGame() {
-            this.running = false;
+            this.running = true;
             this.playerLife = 100;
             this.monsterLife = 100;
+            this.logs = [];
         },
         attack(especial) {
-            this.hurt('monsterLife', 5, 10, especial);
-            this.hurt('playerLife', 7, 12, false);
+            this.hurt('monsterLife', 5, 10, especial, 'Jogador', 'Monstro', 'player');
+            if (this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
+            }            
         },
-        hurt(atr, min, max, especial) {
+        hurt(atr, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0;
             const hurt = this.getRandom(min + plus, max + plus);
             this[atr] = Math.max(this[atr] - hurt, 0);
+            this.registerLog(`${source} atingiu o ${target} com ${hurt}`, cls);
         },
         healAndHurt() {
             this.heal(10, 15);
-            this.hurt('playerLife', 7, 12, false);
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
         },
         heal(min, max) {
-            const heal = this.geRandom(min, max);
+            const heal = this.getRandom(min, max);
             this.playerLife = Math.min(this.playerLife + heal, 100);
+            this.registerLog(`Jogador ganhou força de ${heal}`, 'player');
         },
-        geRandom(min, max) {
+        getRandom(min, max) {
             const value = Math.random() * (max - min) + min;
             return Math.round(value);
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({ text, cls });
         }
     },
     watch: {
