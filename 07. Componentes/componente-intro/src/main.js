059a810c56a8d87b4d81cc1be963ddfa63affc04
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-31 20:54:03 -0300

    Criar um Componente

--- 07. Componentes/componente-intro/src/main.js
+++ 07. Componentes/componente-intro/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Contador from './Contador.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('app-contador', Contador)
+app.mount('#app')
