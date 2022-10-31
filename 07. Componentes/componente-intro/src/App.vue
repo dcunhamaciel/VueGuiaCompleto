bfb092dc2dce5b6cbe5ef297a03b1f04ca0f778e
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-31 20:03:11 -0300

    Criar Projeto Usando Vue CLI

---
+++ 07. Componentes/componente-intro/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <h1>Contadores:</h1>
+        {{ contador }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+          return {
+              contador: 0
+          }
+        }
+    }
+</script>
