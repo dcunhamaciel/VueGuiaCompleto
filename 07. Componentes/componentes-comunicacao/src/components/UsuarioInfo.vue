dd9393f0ab99a6b65c8c81e03e1c770e71096813
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-02 16:18:51 -0300

    Nome das Propriedades são Case-Sensitive

--- 07. Componentes/componentes-comunicacao/src/components/UsuarioInfo.vue
+++ 07. Componentes/componentes-comunicacao/src/components/UsuarioInfo.vue
@@ -2,13 +2,13 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do Usuário: <strong>{{ nome }}</strong></p>
+        <p>Nome do Usuário: <strong>{{ usuarioNome }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['nome']
+    props: ['usuarioNome']
 }
 </script>
 
