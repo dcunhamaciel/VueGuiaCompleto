bc7841138cb56fe6671eb2569ea3d2ec9f656f7b
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-02 14:38:50 -0300

    Hora de Praticar: Organizar Projeto em Componentes

--- 07. Componentes/componente-desafio/src/main.js
+++ 07. Componentes/componente-desafio/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Bar from './components/Bar.vue'
+import Navigation from './components/Navigation.vue'
+import Carousel from './components/Carousel.vue'
+import Footer from './components/Footer.vue'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
+Vue.component('app-bar', Bar)
+Vue.component('app-navigation', Navigation)
+Vue.component('app-carousel', Carousel)
+Vue.component('app-footer', Footer)
 
 new Vue({
   vuetify,
