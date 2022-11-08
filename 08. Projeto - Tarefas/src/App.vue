59389543f4ef825ae97ac2d8d80d229ae31a6e6d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-08 19:18:23 -0300

    Tarefas (TODO)

--- 08. Projeto - Tarefas/src/App.vue
+++ 08. Projeto - Tarefas/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-	</div>
+    <div id="app">
+        <h1>Tarefas</h1>
+    </div>
 </template>
 
 <script>
@@ -11,24 +11,24 @@ export default {
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+    body {
+        font-family: 'Lato', sans-serif;
+        background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+        color: #FFF;
+    }
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+    #app {
+        display: flex;
+        flex: 1;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+    #app h1 {
+        margin-bottom: 5px;
+        font-weight: 300;
+        font-size: 3rem;
+    }
 </style>
