ee98912c16270341edbc0bd0acd6b8ba5378abb4
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-12 11:25:05 -0300

    Componente Tarefa

--- 08. Projeto - Tarefas - Professor/src/components/TaskGrid.vue
+++ 08. Projeto - Tarefas - Professor/src/components/TaskGrid.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="tasks">
+    <div class="task-grid">
         <template>
-            <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
+            <task v-for="task in tasks" :key="task.name" :task="task"></task>
         </template>
     </div>
 </template>
 
 <script>
+import Task from './Task.vue';
+
 export default {
+    components: { Task },
     props: {
         tasks: { 
             type: Array,
@@ -18,5 +21,30 @@ export default {
 </script>
 
 <style>
+    .task {
+        box-sizing: border-box;
+        width: 350px;
+        height: 150px;
+        padding: 10px;
+        border-radius: 8px;
+        font-size: 2rem;
+        font-weight: 300;
+        cursor: pointer;
+        user-select: none;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
+    .pending {
+        border-left: 12px solid #B73229;
+        background-color: #F44336;
+    }
+
+    .done {
+        color: #DDD;
+        border-left: 12px solid #0A8F08;
+        background-color: #4CAF50;
+        text-decoration: line-through;
+    }
 </style>
