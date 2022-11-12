cd5d29ea3dea2ce7f135eb5046779afdf30178ad
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-12 09:49:35 -0300

    Componente ProgressBar

--- 08. Projeto - Tarefas/src/components/ProgressBar.vue
+++ 08. Projeto - Tarefas/src/components/ProgressBar.vue
@@ -1,16 +1,25 @@
 <template>
     <div style="width: 50%">
         <b-progress>
-            <b-progress-bar :value="value" variant="info" :label="`${value}%`"></b-progress-bar>
+            <b-progress-bar :value="tasksDone" variant="info" :label="`${tasksDone}%`"></b-progress-bar>
         </b-progress>
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            value: 50
+    props: {
+        taskList: Array
+    },
+    computed: {
+        tasksDone() {
+            if (this.taskList.length == 0)
+                return 0;
+
+            let taskListDone = this.taskList.filter(task => task.done);
+            let done = (taskListDone.length * 100 / this.taskList.length).toFixed(2);
+
+            return done;
         }
     }
 }
