1811c3a56b34ae2851ec51b9fc4c3120a87d2dbc
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-13 09:12:43 -0300

    Usando Checkboxes e Salvando os Dados em um Array

--- 09. Formulários/formulario-exercicios/src/App.vue
+++ 09. Formulários/formulario-exercicios/src/App.vue
@@ -17,8 +17,8 @@
 					<textarea name="" cols="30" rows="5" v-model="mensagem"></textarea>
 				</Rotulo>
 				<Rotulo nome="Características do Problema">
-					<span class="mr-4"><input type="checkbox" value="reproduzivel"> Reproduzível</span>
-					<span><input type="checkbox" value="intermitente"> Intermitente</span>
+					<span class="mr-4"><input type="checkbox" value="reproduzivel" v-model="caracteristicas"> Reproduzível</span>
+					<span><input type="checkbox" value="intermitente" v-model="caracteristicas"> Intermitente</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
 					<span class="mr-4"><input type="radio"> Web</span>
@@ -51,7 +51,11 @@
 					<span style="white-space: pre;">{{ mensagem }} </span>
 				</Rotulo>
 				<Rotulo nome="Marque as Opções">
-					<span>???</span>
+					<span>
+						<ul>
+							<li v-for="caracteristica in caracteristicas" :key="caracteristica">{{ caracteristica }}</li>
+						</ul>
+					</span>
 				</Rotulo>
 				<Rotulo nome="Qual produto?">
 					<span>???</span>
@@ -77,6 +81,7 @@ export default {
 	data() {
 		return {
 			mensagem: '',
+			caracteristicas: [],
 			usuario: {
 				email: '',
 				senha: '',
