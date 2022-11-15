efb40839b2f90a2d9f84f006a26c14d2e8c8f8f3
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-15 09:43:40 -0300

    Hora de Praticar - Diretivas

--- 10. Diretivas/diretivas-desafio/src/App.vue
+++ 10. Diretivas/diretivas-desafio/src/App.vue
@@ -2,14 +2,44 @@
 	<div id="app">
 		<h1>Diretivas (Desafio)</h1>
 		<hr>
-		<!-- Exercício -->
-		<!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+		<button v-quando-1="executar">Executar - Versão 1</button>
+		<br>
+		<button v-quando-2:click="executar">Executar - Versão 2</button>
+		<br>
+		<p v-quando-2:mouseenter="mouseEnter" v-quando-2:mouseleave="mouseLeave">Teste mouse event!</p>
 	</div>
 </template>
 
 <script>
 export default {
-	
+	directives: {
+		'quando-1': {
+			bind(el, binding) {
+				el.onclick = function() {
+					binding.value();
+				}	
+			}
+		},
+		'quando-2': {
+			bind(el, binding) {
+				const tipo = binding.arg;
+				const fn = binding.value;
+
+				el.addEventListener(tipo, fn);
+			}
+		}
+	},
+	methods: {
+		executar() {
+			alert('executado!');
+		},
+		mouseEnter() {
+			console.log('Mouse Enter!');
+		},
+		mouseLeave() {
+			console.log('Mouse Leave!');
+		}
+	}
 }
 </script>
 
