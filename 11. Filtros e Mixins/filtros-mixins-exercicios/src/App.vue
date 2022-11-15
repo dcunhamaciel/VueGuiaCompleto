acfdeb96b5c406114289a89d140cec3899aee57a
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-15 10:23:17 -0300

    Filtro & v-bind

--- 11. Filtros e Mixins/filtros-mixins-exercicios/src/App.vue
+++ 11. Filtros e Mixins/filtros-mixins-exercicios/src/App.vue
@@ -3,6 +3,7 @@
 		<h1>Filtros & Mixins</h1>
 		<hr>
 		<p>{{ cpfAluno | cpf | inverter }}</p>
+		<input type="text" :value="cpfAluno | cpf">
 	</div>
 </template>
 
@@ -36,5 +37,9 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 2.5rem;
+}
+
+input {
+	font-size: 2.5rem;	
 }
 </style>
