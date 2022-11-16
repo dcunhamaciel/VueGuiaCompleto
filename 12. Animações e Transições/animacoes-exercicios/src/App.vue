11a004b33467c95d8e40314f0992fdf2754f213a
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-16 19:39:16 -0300

    Usando v-show

--- 12. Animações e Transições/animacoes-exercicios/src/App.vue
+++ 12. Animações e Transições/animacoes-exercicios/src/App.vue
@@ -3,11 +3,13 @@
 		<h1>Animações</h1>
 		<hr>
 		<b-button variant="primary" class="mb-4" @click="exibir = !exibir">Mostrar Mensagem</b-button>
+
 		<transition name="fade">
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
+
 		<transition name="slide">
-			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
 		</transition>		
 	</div>
 </template>
