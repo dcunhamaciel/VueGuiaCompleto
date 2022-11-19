281c443688cb5377424894d81fbf76c9be7d828f
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-19 16:40:30 -0300

    Configuração do vue-router

---
+++ 14. Rotas/rotas-exercicios/src/components/Inicio.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="inicio">
+        <h2>Início</h2>
+    </div>    
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ 14. Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="usuario">
+        <h2>Usuários</h2>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
