a41036c5736eb8f943fe1c7c1d5a1a205b8a962d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-19 10:09:49 -0300

    Criando Instância do Axios

--- 13. Conectando com Servidor via HTTP/http-exercicios/src/plugins/axios.js
+++ 13. Conectando com Servidor via HTTP/http-exercicios/src/plugins/axios.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Axios from 'axios'
 
-Axios.defaults.baseURL = 'https://curso-vue-3e445-default-rtdb.firebaseio.com/'
+//Axios.defaults.baseURL = 'https://curso-vue-3e445-default-rtdb.firebaseio.com/'
 
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = Axios
+        //Vue.prototype.$http = Axios
+        Vue.prototype.$http = Axios.create({
+            baseURL: 'https://curso-vue-3e445-default-rtdb.firebaseio.com/'
+        })
     }
 })
