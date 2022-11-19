56e5d0391e90794e3bdc2d19fa2aa50a8ac8733b
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-19 11:12:52 -0300

    dicionando Headers Globais

--- 13. Conectando com Servidor via HTTP/http-exercicios/src/plugins/axios.js
+++ 13. Conectando com Servidor via HTTP/http-exercicios/src/plugins/axios.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Axios from 'axios'
 
-//Axios.defaults.baseURL = 'https://curso-vue-3e445-default-rtdb.firebaseio.com/'
+// Axios.defaults.baseURL = 'https://curso-vue-3e445-default-rtdb.firebaseio.com/'
+// Axios.defaults.headers.common['Authorization'] = 'abc123';
+// Axios.defaults.headers.get['Accepts'] = 'application/json';
 
 Vue.use({
     install(Vue) {
         //Vue.prototype.$http = Axios
         Vue.prototype.$http = Axios.create({
-            baseURL: 'https://curso-vue-3e445-default-rtdb.firebaseio.com/'
+            baseURL: 'https://curso-vue-3e445-default-rtdb.firebaseio.com/',
+            headers: {
+                "Authorization": "abc123"
+            }
         })
 
         Vue.prototype.$http.interceptors.request.use(config => {
