631552f7bb820944d335e6e0aa431559625ba124
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-19 10:16:44 -0300

    Criando Formulário

--- 13. Conectando com Servidor via HTTP/http-exercicios/src/App.vue
+++ 13. Conectando com Servidor via HTTP/http-exercicios/src/App.vue
@@ -1,11 +1,34 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o Nome"></b-form-input>				
+			</b-form-group>
+			<b-form-group label="E-mail:">		
+				<b-form-input type="text" size="lg" v-model="usuario.email" placeholder="Informe o E-mail"></b-form-input>
+			</b-form-group>						
+		</b-card>
+		<hr>
+		<b-button @click="salvar" size="lg" variant="primary">Salvar</b-button>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		salvar() {
+			console.log(this.usuario);
+		}
+	}
 	// created() {
 	// 	this.$http.post('usuarios.json', {
 	// 		nome: 'Maria',
