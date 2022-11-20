41ffb9a8d4b619b171476c76d4e02f611651f8d7
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-20 11:01:40 -0300

    Configurando Rotas Filhas (Rotas Aninhadas)

--- 14. Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ 14. Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,24 +2,13 @@
     <div class="usuario">
         <h2>Usuários</h2>
         <hr>
-        <p><strong>Código: </strong>{{ id }}</p>
+        <router-view></router-view>        
         <button @click="irParaInicio" sucesso>Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // },
-    // watch: {
-    //     $route(to, from) {
-    //         this.id = to.params.id;
-    //     }
-    // },
     methods: {
         irParaInicio() {
             this.$router.push('/');
--- 14. Rotas/rotas-exercicios/src/router.js
+++ 14. Rotas/rotas-exercicios/src/router.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Inicio from './components/Inicio.vue';
+
 import Usuario from './components/usuario/Usuario.vue';
+import UsuarioDetalhe from './components/usuario/UsuarioDetalhe.vue';
+import UsuarioEditar from './components/usuario/UsuarioEditar.vue';
+import UsuarioLista from './components/usuario/UsuarioLista.vue';
 
 Vue.use(Router)
 
@@ -11,8 +15,13 @@ export default new Router({
         path: '/',
         component: Inicio
     }, {
-        path: '/usuario/:id',
+        path: '/usuario',
         component: Usuario,
-        props: true
+        props: true,
+        children: [
+            { path: '', component: UsuarioLista },
+            { path: ':id', component: UsuarioDetalhe, props: true },
+            { path: ':id/editar', component: UsuarioEditar, props: true }
+        ]
     }]
 })
