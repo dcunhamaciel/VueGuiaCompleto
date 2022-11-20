8155bca177812c3ba6edb163b141d2766d262b42
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-20 09:59:41 -0300

    Navegação via Código (Navegação Imperativa)

--- 14. Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
+++ 14. Rotas/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="usuario">
         <h2>Usuários</h2>
+        <hr>
+        <button @click="irParaInicio" sucesso>Voltar</button>
     </div>
 </template>
 
 <script>
 export default {
+    methods: {
+        irParaInicio() {
+            this.$router.push('/');
+            //this.$router.push( { path: '/' } );
+        }
+    }
 }
 </script>
