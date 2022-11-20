a300f6b92132dc85e5eee8a2fe28bd8d4646f55f
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-20 11:06:08 -0300

    Navegando para Rotas Aninhadas

--- 14. Rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ 14. Rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="usuario-editar">
         <h3>Usuário Editar</h3>
+        <p><strong>Código: </strong>{{ id }}</p>
     </div>
 </template>
 
 <script>
 export default {
+    props: ['id']
 }
 </script>
