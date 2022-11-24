f2bd44b9b0c55d0e75c01e1069ee851c9edbe54e
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-24 18:23:49 -0300

    Usando Arquivos Separados

--- 15. VueX/vuex-exercicios/src/store/store.js
+++ 15. VueX/vuex-exercicios/src/store/store.js
@@ -4,8 +4,18 @@ import Vuex from 'vuex';
 import carrinho from './modules/carrinho';
 import parametros from './modules/parametros';
 
+import * as getters from './getters';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    state: {
+        nome: 'Maria',
+        sobrenome: 'Silva'
+    },
+    // getters,
+    getters: {
+        ...getters
+    },
     modules: { carrinho, parametros }
 })
