0c75530168dd93f3982df7ce0f6ece004bc0829b
dcunhamaciel <dcunhamaciel@gmail.com>
2022-11-24 16:59:12 -0300

    Modularizando o Gerenciador de Estado

---
+++ 15. VueX/vuex-exercicios/src/store/modules/carrinho.js
@@ -0,0 +1,29 @@
+export default {
+    state: {
+        produtos: []
+    },
+    getters: {
+        valorTotal(state) {
+            return state.produtos
+                .map(p => p.quantidade * p.preco)
+                .reduce((total, atual) => total + atual, 0);
+        }
+    },
+    mutations: {
+        adicionarProduto(state, payload) {
+            state.produtos.push(payload);
+        }
+    },
+    actions: {
+        // adicionarProduto(context, payload) {
+        //     setTimeout(() => {
+        //         context.commit('adicionarProduto', payload);
+        //     }, 1000);
+        // }
+        adicionarProduto({ commit }, payload) {
+            setTimeout(() => {
+                commit('adicionarProduto', payload);
+            }, 1000);
+        }        
+    }
+}
